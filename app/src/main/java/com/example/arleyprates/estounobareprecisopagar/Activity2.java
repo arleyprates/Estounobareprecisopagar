da6303e02ab1740ffe7d0b4ecd9ea43a3ac3fd36
Arley Prates <arleyprates@gmail.com>
2016-04-03 00:36:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/arleyprates/estounobareprecisopagar/Activity2.java
@@ -0,0 +1,17 @@
+package com.example.arleyprates.estounobareprecisopagar;
+
+/**
+ * Created by arleyprates on 4/2/16.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity2 extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
